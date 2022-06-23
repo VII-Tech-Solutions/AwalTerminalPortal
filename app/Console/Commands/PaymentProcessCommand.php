fbc9035501ad7530a3c7431ebe27628313c7c9a2
Ahmed Yusuf <ahmed.yusuf@viitech.net>
2022-06-23 11:42:59 +0300

    working on payment process

--- app/Console/Commands/PaymentProcessCommand.php
+++ app/Console/Commands/PaymentProcessCommand.php
@@ -13,9 +13,11 @@ use Illuminate\Console\Command;
  */
 class PaymentProcessCommand extends Command
 {
-    protected $signature = 'process:payment';
+    protected $signature = 'process:payment {email?}';
 
     protected $description = 'Payment Process Command';
+
+    public $email;
 
     /**
      * Handle
@@ -23,9 +25,16 @@ class PaymentProcessCommand extends Command
     public function handle()
     {
 
+        $this->email = $this->argument("email");
+
         // get elite service
         /** @var EliteServices $elite_service */
-        $elite_service = EliteServices::where(Attributes::STATUS, ESStatus::PENDING_APPROVAL)->orderByDesc(Attributes::CREATED_AT)->first();
+        $elite_service = EliteServices::where(Attributes::SUBMISSION_STATUS_ID, ESStatus::PENDING_APPROVAL)->orderByDesc(Attributes::CREATED_AT)->first();
+
+        // validate elite service
+        if(is_null($elite_service)){
+            dd("Elite service is null");
+        }
 
         // change status
         $this->changeStatus($elite_service);
@@ -38,18 +47,23 @@ class PaymentProcessCommand extends Command
      */
     function changeStatus($elite_service){
 
+
         // get booker
         /** @var Bookers $booker */
-        $booker = $elite_service->bookers()->first();
+        $booker = $elite_service->booker()->first();
+
+        if(empty($this->email)){
+            $this->email = $booker->email;
+        }
 
         // pending approval
-        EliteServices::changeStatus($elite_service->id, $booker->first_name, $booker->email, ESStatus::PENDING_APPROVAL, null);
+        EliteServices::changeStatus($elite_service->id, $booker->first_name, $this->email, ESStatus::PENDING_APPROVAL, null);
 
         // rejection
-        EliteServices::changeStatus($elite_service->id, $booker->first_name, $booker->email, ESStatus::REJECTED, "Just Because");
+        EliteServices::changeStatus($elite_service->id, $booker->first_name, $this->email, ESStatus::REJECTED, "Just Because");
 
         // approved
-        EliteServices::changeStatus($elite_service->id, $booker->first_name, $booker->email, ESStatus::APPROVED, null);
+        EliteServices::changeStatus($elite_service->id, $booker->first_name, $this->email, ESStatus::APPROVED, null);
 
     }
 }
