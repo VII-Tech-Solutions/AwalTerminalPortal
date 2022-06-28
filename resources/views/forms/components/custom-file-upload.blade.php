a951d9a7264cbb0b9b96dab06058a9b2ba0d2114
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-06-28 19:54:24 +0300

    attempted to change table style

--- resources/views/forms/components/custom-file-upload.blade.php
+++ resources/views/forms/components/custom-file-upload.blade.php
@@ -1,14 +1,17 @@
 @php
     $attachments = \App\Models\Attachment::where(\App\Constants\Attributes::FORM_ID, $this->record->id)->get();
 @endphp
-<table>
+<table class="table-auto border-separate border-spacing-2 " >
     <tbody>
     @foreach($attachments as $attachment)
-        <tr role="row">
+        <tr class="row-auto">
             <td style="vertical-align: middle;" width="450px">{{ $attachment->file_label }}</td>
             <td>
-                <button class="font-bold py-2 px-4 rounded inline-flex items-center" onclick="window.open('{{ $attachment->url }}', '_blank')">
-                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                <button class="font-bold py-2 px-4 rounded inline-flex items-center"
+                        onclick="window.open('{{ $attachment->url }}', '_blank')">
+                    <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+                    </svg>
                     <span>Download</span>
                 </button>
             </td>
