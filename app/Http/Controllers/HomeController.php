fac0a5e56fe219275ec2aaf73865f8a61adfe960
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-08-02 15:06:45 +0300

    changed auth information

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -219,7 +219,7 @@ class HomeController extends CustomController
 
             $url = env('PAYMENT_URL') . '/benefit/checkout';
 
-            $client = new Client(['auth' => ['awal', 'password']]);
+            $client = new Client(['auth' => ['Test@123', 'password']]);
 
             $response = $client->request('POST', $url, [
                 'multipart' => $benefit_request_data
