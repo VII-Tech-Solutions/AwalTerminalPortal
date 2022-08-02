f7bb3e696df2d25459029507ab221759535d2960
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-08-02 17:50:47 +0300

    attempting to fix the deployment error

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.postCss('resources/css/filament.css', 'public/css', [
-    require('tailwindcss'),
-])
+// mix.postCss('resources/css/filament.css', 'public/css', [
+//     require('tailwindcss'),
+// ])
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
