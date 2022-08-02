1e5c12d0665472bae0671c93050dfe6634e26d74
fatima <>
2022-08-02 11:18:38 +0300

    Elite services Request Received mail template

--- resources/views/emails/ga_request_received.blade.php
+++ resources/views/emails/ga_request_received.blade.php
@@ -14,18 +14,15 @@
     </div>
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41"> Thanks
-            for your interest in Awal Private Terminal. We have received your request and it is under review.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41"> Thanks for your interest in Awal Private Terminal. We have received your request and it is under review.</p>
     </div>
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">A
-            member of our team is reviewing your submission and will get in contact with you shortly.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">A member of our team is reviewing your submission and will get in contact with you shortly.</p>
     </div>
 
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Here is
-            an overview of all the details you submitted:</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Here is an overview of all the details you submitted:</p>
     </div>
 
     [Booking details]
@@ -39,9 +36,7 @@
     <br>
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">If you
-            require any further assistance, please contact the Bookings Team on Tel: +973 17139831 or Mobile: +973
-            39471116 or mail us at elite@halabahrain.bh.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">If you require any further assistance, please contact the Bookings Team on Tel: +973 17139831 or Mobile: +973 39471116 or mail us at elite@halabahrain.bh.</p>
     </div>
     <br>
 @endsection
