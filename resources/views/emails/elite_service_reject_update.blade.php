66b69d0c1f663d903cfac7f5ab9b55b3fd446d10
fatima <>
2022-08-02 10:32:08 +0300

    Reject email template

--- resources/views/emails/elite_service_reject_update.blade.php
+++ resources/views/emails/elite_service_reject_update.blade.php
@@ -8,23 +8,19 @@
     </div>
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Your
-            booking for Elite Service at Awal Private Terminal has been rejected. </p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Your booking for Elite Service at Awal Private Terminal has been rejected.</p>
     </div>
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">The
-            reason provided by management is the following: </p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">The reason provided by management is the following </p>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #FF0000"> {{$rejectionReason}}</p>
     </div>
 
     <br>
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">You can
-            resubmit a request on our website and try again.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">You can resubmit a request on our website and try again..</p>
     </div>
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">We
-            appreciate your understanding.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">We appreciate your understanding.</p>
     </div>
     <br>
 @endsection
--- resources/views/emails/elite_service_request_received.blade.php
+++ resources/views/emails/elite_service_request_received.blade.php
@@ -1,7 +1,5 @@
 @extends('emails.layout')
 @section('content')
-    <p style="font-size: 20px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Elite Service Request</p>
-
     <div style="display: flex">
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Hi &nbsp;</p>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #4e89e8">{{$to_name}}</p>
@@ -9,7 +7,7 @@
     </div>
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Thanks for your interest in Awal Private Terminal. We have received your request and it is under review.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Thanks for your interest in Awal Private Terminal. We are pleased to receive your request and to confirm that it is being processed.</p>
     </div>
     <div>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Once it is approved, we will send you an email to complete the payment process and reserve your booking.</p>
@@ -17,17 +15,67 @@
 
     <br>
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Here is an overview all the details you submitted:</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Here is an overview of all the details you submitted:</p>
     </div>
-    {{--    [Booking details]--}}
+    <div>
+        Flight Status: Arrival
+    </div>
+    <div>
+        Arriving From: Dubai International Airport
+    </div>
+    <div>
+        Date: 24 April, 2022
+    </div>
+    <div>
+        Time: 4E89E8
+    </div>
+    <div style="padding-bottom: 20px">
+        Flight Number: BA2490A
+    </div>
+
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">The total amount you are required to pay is BHD {{$data[0]}}.</p>
+        Adults: 2
+    </div>
+    <div>
+        Children: 1
+    </div>
+    <div style="padding-bottom: 20px">
+        Infants: 0
+    </div>
+
+
+    <div>
+        Passenger 1
+    </div>
+    <div>
+        Title: Mr.
+    </div>
+    <div>
+        First Name: Ahmad
+    </div>
+    <div>
+        Last Name: Yusuf
+    </div>
+    <div>
+        Date of Birth: 27 October, 1991
+    </div>
+    <div>
+        Nationality: Bahraini
+    </div>
+    <div style="padding-bottom: 20px">
+        Class: Economy
+    </div>
+
+
+
+    <div>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">The total amount you are required to pay is BHD{{$data[0]}}.</p>
     </div>
     <br>
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">If you require any further assistance, please contact the Bookings Team.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">If you require any further assistance, please contact the Bookings Team on Tel: +973 17139831 or Mobile: +973 39471116 or mail us at elite@halabahrain.bh.</p>
     </div>
     <br>
 
