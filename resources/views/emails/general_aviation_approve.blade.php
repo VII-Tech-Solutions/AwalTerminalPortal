81f8e59a13867c64584d45438decc5b739a9ea23
fatima <>
2022-08-03 11:23:29 +0300

    Working on General aviation submission approved

--- resources/views/emails/general_aviation_approve.blade.php
+++ resources/views/emails/general_aviation_approve.blade.php
@@ -3,9 +3,12 @@
     <div style="display: flex">
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Hi &nbsp;</p>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #4e89e8">{{$to_name}}
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">&nbsp; or &nbsp; </p>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #4e89e8">{{$data[0]}}</p>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">,</p>
+        @if($data[0])
+            <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">&nbsp;
+                or &nbsp;</p>
+            <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #4e89e8">{{$data[0]}}</p>
+            <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">,</p>
+        @endif
     </div>
 
     <div>
@@ -160,6 +163,21 @@
 
     @endif
 
+    @if($data[28])
+        <div>
+            <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">List of Services:
+                @foreach($data[28] as $key => $value)
+                    @if($key != 0)
+                        ,
+                    @endif
+                    {{$value['name']}}
+                @endforeach
+            </p>
+        </div>
+    @endif
+    <br>
+
+    <br>
     <div>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">If you require any further assistance, please contact the Bookings Team on Tel: +973 17139831 or Mobile: +973 39471116 or mail us at elite@halabahrain.bh.</p>
     </div>
