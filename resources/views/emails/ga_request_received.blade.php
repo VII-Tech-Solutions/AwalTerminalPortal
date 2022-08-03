b14cdc23e659714d44fc89d11f5e5e6af8247cf3
fatima <>
2022-08-03 09:33:04 +0300

    Working on GA services Request Received mail template

--- resources/views/emails/ga_request_received.blade.php
+++ resources/views/emails/ga_request_received.blade.php
@@ -173,9 +173,9 @@
     <div>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">List of Services:
 {{--            {{$data[28]}}--}}
-{{--            @foreach($data[28] as $value)--}}
-{{--            {{$value}} ,--}}
-{{--            @endforeach--}}
+            @foreach($data[28] as $value)
+            {{$value}} ,
+            @endforeach
 
         </p>
     </div>
