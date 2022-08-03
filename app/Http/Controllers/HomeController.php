1f51ae525e2e4b65ef874321221abdbe560be1ea
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-08-03 12:03:40 +0300

    attempting to fix the server issue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -126,7 +126,6 @@ class HomeController extends CustomController
 
         // get uuid
         if (empty($uuid)) {
-            dd("empty uuid");
             return redirect()->to(env("WEBSITE_URL") . "/link-expired");
         }
 
@@ -134,7 +133,6 @@ class HomeController extends CustomController
         /** @var EliteServices $elite_service */
         $elite_service = EliteServices::where(Attributes::UUID, $uuid)->first();
         if (is_null($elite_service)) {
-            dd("empty elite service");
             return redirect()->to(env("WEBSITE_URL") . "/link-expired");
         }
 
@@ -173,12 +171,13 @@ class HomeController extends CustomController
 
         } else if ($payment_method == PaymentProvider::BENEFIT) {
 
-            // TODO implement benefit
-            $success_url = "/payment-received";
-            $error_url = "/payment-failed";
+            $secret = env("PAYMENT_SECRET", 'FzpTv!dEiVC_i.Cp7nQgQH-UWW63LE_tdVtUA9v4Xr!uum6tcJ');
+            $success_url = url("/api/payments/verify-benefit?booking=$elite_service->uuid&secret=$secret&platform=web");
+            $error_url = url("/api/payments/verify-benefit?booking=$elite_service->uuid&secret=$secret&platform=web");
+
             $booker = $elite_service->booker()->first();
 
-            $name = $booker->first_name ." ". $booker->last_name;
+            $name = $booker->first_name . " " . $booker->last_name;
             $phoneNumber = $booker->mobile_number;
 
             // go to payment page
