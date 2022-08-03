d03c981298f38c405568f218209fcd26c49e70b9
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-08-03 12:11:22 +0300

    attempting to fix the server issue

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -211,8 +211,8 @@ class HomeController extends CustomController
                 Attributes::CUSTOMER_PHONE_NUMBER => $customer_phone_number,
                 Attributes::PAYMENT_SECRET => env("PAYMENT_SECRET", 'FzpTv!dEiVC_i.Cp7nQgQH-UWW63LE_tdVtUA9v4Xr!uum6tcJ'),
                 Attributes::BENEFIT_MIDDLEWARE_TOKEN => env("PAYMENT_SECRET", 'FzpTv!dEiVC_i.Cp7nQgQH-UWW63LE_tdVtUA9v4Xr!uum6tcJ'),
-                Attributes::SUCCESS_URL => $success_url,
-                Attributes::ERROR_URL => $error_url,
+//                Attributes::SUCCESS_URL => $success_url,
+//                Attributes::ERROR_URL => $error_url,
                 Attributes::MERCHANT_ID => env("BENEFIT_MERCHANT_ID", "12818950")
             ];
 
