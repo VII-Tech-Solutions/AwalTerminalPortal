e6ea08bb046b431cf5b152498259ecb54cfb1f61
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-08-07 11:55:15 +0300

    payment fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -162,7 +162,7 @@ class HomeController extends CustomController
             $query = http_build_query([
                 Attributes::RETURN_URL => url("elite-service/$uuid/pay/complete"),
                 Attributes::AMOUNT => $transaction->amount,
-                Attributes::ORDER_ID => $transaction->uuid,
+                Attributes::ORDER_ID => $transaction->order_id,
                 Attributes::DESCRIPTION => "Awal Private Terminal Elite Services",
             ]);
 
