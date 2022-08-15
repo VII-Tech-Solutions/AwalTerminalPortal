623b9cf82ffcd4ffde5bd9ed4c3c0cbf9e9b45a9
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-08-15 16:27:08 +0300

    benefit payment fixes

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -204,8 +204,7 @@ class PaymentController extends CustomController
 
             $url = env('PAYMENT_URL') . '/benefit/checkout';
 
-//            $client = new Client(['auth' => ['awal', 'password']]);
-            $client   = new Client();
+            $client = new Client(['auth' => ['forge', 'password']]);
 
             $response = $client->request('POST', $url, [
                 'multipart' => $benefit_request_data
