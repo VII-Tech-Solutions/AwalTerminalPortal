5eecf5958667ffebc44b59d9c000939df5a61c26
Maymoona <maymoona.alsharqi@viitech.net>
2022-08-22 17:40:36 +0300

    proxies change 2

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,7 +13,7 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies=[
-           '10.240.16.94'
+           '*'
         ];
 
     /**
