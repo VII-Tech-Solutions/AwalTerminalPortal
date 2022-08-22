403b762c9f2535e062664b3827889596b33e1012
Maymoona <maymoona.alsharqi@viitech.net>
2022-08-22 19:09:58 +0300

    ip address

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies='*';
+    protected $proxies=['10.240.16.94'];
 
     /**
      * The headers that should be used to detect proxies.
