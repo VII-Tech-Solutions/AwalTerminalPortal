4253b944ca647f5c922337d82beef897d9354e45
Ahmed Ebrahim <ahmed.ebrahim@viitech.net>
2022-08-23 14:11:16 +0300

    email, approved changed to confirmed

--- resources/views/emails/elite_service_approve.blade.php
+++ resources/views/emails/elite_service_approve.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <div>
-        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Your booking for Elite Service at Awal Private Terminal has been approved.</p>
+        <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Your booking for Elite Service at Awal Private Terminal has been confirmed.</p>
     </div>
     <div>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">The total amount you are required to pay is BHD{{$data[1]}}</p>
