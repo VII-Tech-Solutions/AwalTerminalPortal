fd49a2364cc7748647e47c3b10e0020576ce7b63
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-12 11:42:13 +0300

    payment redirection fixes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -142,7 +142,7 @@ class HomeController extends CustomController
 
             // build url query
             $query = http_build_query([
-//                Attributes::RETURN_URL => url("/payments/verify-credimax?booking=$transaction->uuid&secret=$secret&platform=web"),
+                Attributes::RETURN_URL => $error_url,
                 Attributes::SUCCESS_URL => $success_url,
                 Attributes::ERROR_URL => $error_url,
                 Attributes::AMOUNT => $transaction->amount,
