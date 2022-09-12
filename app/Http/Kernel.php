c32b91ec209d1a27f86eccddb4ba852e991b236c
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-12 15:38:55 +0300

    cors fix and email fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,14 +34,18 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustHosts::class,
         \Fruitcake\Cors\HandleCors::class,
         TrustProxies::class,
-        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         ValidatePostSize::class,
         TrimStrings::class,
         ConvertEmptyStringsToNull::class,
+        \Fruitcake\Cors\HandleCors::class,
+        \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
@@ -60,7 +64,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             SubstituteBindings::class,
         ],
--- resources/views/emails/ga_service_new_request.blade.php
+++ resources/views/emails/ga_service_new_request.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div style="display: flex">
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Hi there,</p>
-        @endif
+
     </div>
     <div>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41"> A new General Aviation request has been submitted.</p>
