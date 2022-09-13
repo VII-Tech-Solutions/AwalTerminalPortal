8bf9430e2b1e08711422ba78401e25f71a62a6f3
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-13 16:31:25 +0300

    cors fix and email fix

--- resources/views/emails/elite_service_new_booking.blade.php
+++ resources/views/emails/elite_service_new_booking.blade.php
@@ -3,7 +3,6 @@
 
     <div style="display: flex">
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41">Hi there,</p>
-        @endif
     </div>
     <div>
         <p style="font-size: 16px; line-height: 27px;font-family: 'Source Sans Pro', sans-serif; color: #011e41"> A new Elite Service booking has been submitted.</p>
