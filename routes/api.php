628df03690774970bb77f00e15f1efaf64add4a6
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-13 21:29:43 +0300

    cors fix

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@
 /** @var Router $api */
 
 use Dingo\Api\Routing\Router;
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
+header('Access-Control-Allow-Origin: *');
 
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
