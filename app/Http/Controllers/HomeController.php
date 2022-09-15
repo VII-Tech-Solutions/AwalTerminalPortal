12e2ad361f551d59e82bde1f629afd1c524ca79b
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-15 09:37:34 +0300

    payment debugging

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -167,6 +167,7 @@ class HomeController extends CustomController
 
             // go to payment page
             $payment_url = PaymentController::generateBenefitPaymentLink($transaction->amount, $elite_service->uuid, $name, $phoneNumber, $success_url, $error_url, $transaction->order_id);
+            dd($payment_url);
             return redirect()->to($payment_url);
         }
 
