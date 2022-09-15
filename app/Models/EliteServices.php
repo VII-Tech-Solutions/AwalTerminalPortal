04e5eb0076783ec8fe9fcea19263adf5809f573b
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-15 12:30:21 +0300

    debugging the change of status

--- app/Models/EliteServices.php
+++ app/Models/EliteServices.php
@@ -217,7 +217,7 @@ class EliteServices extends CustomModel
                 $from_airport_id = Airport::query()::where(Attributes::ID, $elite_service->airport_id)->first();
                 $passengers = $elite_service->passengers();
 
-//                Helpers::sendMailable(new ESRequestReceivedMail($booker->email, $booker->first_name.' '. $booker->last_name, [$total, $is_arrival_flight, $date, $time, $flight_number, $number_of_adults, $number_of_children, $number_of_infants, $passengers, $from_airport_id]), $booker->email);
+                Helpers::sendMailable(new ESRequestReceivedMail($booker->email, $booker->first_name . ' ' . $booker->last_name, [$total, $is_arrival_flight, $date, $time, $flight_number, $number_of_adults, $number_of_children, $number_of_infants, $passengers, $from_airport_id]), $booker->email);
                 break;
             case ESStatus::REJECTED:
                 Helpers::sendMailable(new ESBookingRejectUpdateMail($email, $name, $rejection_reason, []), $email);
@@ -239,6 +239,7 @@ class EliteServices extends CustomModel
                 $elite_service->link_expires_at = null;
                 $elite_service->submission_status_id = ESStatus::PAID;
                 $elite_service->save();
+                $booker = $elite_service->booker()->first();
 
                 $transaction = Transaction::query()->where(Attributes::UUID, $elite_service->uuid)->where(Attributes::STATUS, TransactionStatus::SUCCESS)->first();
                 if (is_null($transaction)) {
@@ -255,10 +256,10 @@ class EliteServices extends CustomModel
                         Attributes::AMOUNT,
                     ]);
                 }
+                $data = $transaction->generateReceiptData();
 
                 // send email
-                Helpers::sendMailable(new PaymentCompleted($user->email, $user->first_name, [$elite_service->total], 'receipt.pdf', $transaction->id, $transaction), $user->email);
-
+                Helpers::sendMailable(new PaymentCompleted($email, $booker->first_name . ' ' . $booker->last_name, [$transaction->amount], null, $transaction->id, $transaction, $data), $booker->email);
                 break;
         }
     }
