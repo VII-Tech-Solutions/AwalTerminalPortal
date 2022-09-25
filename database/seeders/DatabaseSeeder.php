c1d7532c997032925444b24b4cad9b8ae80a4ac3
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-25 12:10:37 +0300

    pushing website

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,15 +20,15 @@ class DatabaseSeeder extends Seeder
         $this->call(EliteServiceTypeSeeder::class);
         $this->call(EliteServiceFeatureSeeder::class);
         $this->call(SubmissionStatusSeeder::class);
-//        $this->call(ContectUsContentSeeder::class);
-//        $this->call(HomepageContentSeeder::class);
-//        $this->call(OurStoryContentSeeder::class);
-//        $this->call(BulletPointsContentSeeder::class);
-//        $this->call(ImageGalleryContentSeeder::class);
-//        $this->call(GeneralAviationContentSeeder::class);
-//        $this->call(TourTheTerminalContentSeeder::class);
-//        $this->call(ServicesContentSeeder::class);
-//        $this->call(EliteServicesContentSeeder::class);
+        $this->call(ContectUsContentSeeder::class);
+        $this->call(HomepageContentSeeder::class);
+        $this->call(OurStoryContentSeeder::class);
+        $this->call(BulletPointsContentSeeder::class);
+        $this->call(ImageGalleryContentSeeder::class);
+        $this->call(GeneralAviationContentSeeder::class);
+        $this->call(TourTheTerminalContentSeeder::class);
+        $this->call(ServicesContentSeeder::class);
+        $this->call(EliteServicesContentSeeder::class);
         $this->call(CountrySeeder::class);
         $this->call(AirportsSeeder::class);
     }
