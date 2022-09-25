d54063b3292f8cedb9f221b2df350e482a3074e9
Tasleem AlSharqi <tasleem.alsharqi@viitech.net>
2022-09-25 14:51:20 +0300

    pushing website

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,9 @@ use App\Models\TourTheTerminalContent;
 use App\Models\User;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationItem;
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -35,62 +36,63 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        if($this->app->environment('production')){
+        if ($this->app->environment('production')) {
             URL::forceScheme('https');
         };
 
-        $homePageContentId = HomepageContent::all()->first()->id;
-        $tourPageContentId = TourTheTerminalContent::all()->first()->id;
-        $OurStoryPageContentId = OurStoryContent::all()->first()->id;
-        $servicesPageContentId = ServicesContent::all()->first()->id;
-        $generalAviationPageContentId = GeneralAviationContent::all()->first()->id;
-        $eliteServicesPageContentId = EliteServicesContent::all()->first()->id;
-        $contactUsPageContentId = ContactUsContent::all()->first()->id;
+        $homePageContent = HomepageContent::all()->first();
+        $tourPageContent = TourTheTerminalContent::all()->first();
+        $OurStoryPageContent = OurStoryContent::all()->first();
+        $servicesPageContent = ServicesContent::all()->first();
+        $generalAviationPageContent = GeneralAviationContent::all()->first();
+        $eliteServicesPageContent = EliteServicesContent::all()->first();
+        $contactUsPageContent = ContactUsContent::all()->first();
+        if (!is_null($homePageContent) && !is_null($tourPageContent) && !is_null($OurStoryPageContent) && !is_null($servicesPageContent) && !is_null($generalAviationPageContent) && !is_null($eliteServicesPageContent) && !is_null($contactUsPageContent)) {
+            Filament::serving(function () use ($homePageContent, $tourPageContent, $OurStoryPageContent, $servicesPageContent, $generalAviationPageContent, $eliteServicesPageContent, $contactUsPageContent) {
+                $appUrl = ENV('APP_URL');
+                Filament::registerNavigationGroups([
+                    '',
+                    'Submissions',
+                    'Metadata',
+                ]);
+                /** @var User $user */
+                $user = auth()->user();
+                if (!is_null($user)) {
+                    if ($user->canAccess(AdminUserType::MODERATOR)) {
+                        Filament::registerNavigationItems([
+                            NavigationItem::make()->label('Homepage')
+                                ->url("$appUrl/admin/homepage-contents/$homePageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
+                            NavigationItem::make()->label('Tour page')
+                                ->url("$appUrl/admin/tour-the-terminal-contents/$tourPageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
+                            NavigationItem::make()->label('Our story page')
+                                ->url("$appUrl/admin/our-story-contents/$OurStoryPageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
+                            NavigationItem::make()->label('Services page')
+                                ->url("$appUrl/admin/services-contents/$servicesPageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
+                            NavigationItem::make()->label('General aviation page')
+                                ->url("$appUrl/admin/general-aviation-contents/$generalAviationPageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
+                            NavigationItem::make()->label('Elite services page')
+                                ->url("$appUrl/admin/elite-services-contents/$eliteServicesPageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
+                            NavigationItem::make()->label('Contact us page')
+                                ->url("$appUrl/admin/contact-us-contents/$contactUsPageContent->id/edit")
+                                ->icon('heroicon-o-collection')
+                                ->group("Website Content"),
 
-        Filament::serving(function () use ($homePageContentId, $tourPageContentId, $OurStoryPageContentId, $servicesPageContentId, $generalAviationPageContentId, $eliteServicesPageContentId, $contactUsPageContentId) {
-            $appUrl = ENV('APP_URL');
-            Filament::registerNavigationGroups([
-                '',
-                'Submissions',
-                'Metadata',
-            ]);
-            /** @var User $user */
-            $user = auth()->user();
-            if (!is_null($user)) {
-                if ($user->canAccess(AdminUserType::MODERATOR)) {
-                    Filament::registerNavigationItems([
-                        NavigationItem::make()->label('Homepage')
-                            ->url("$appUrl/admin/homepage-contents/$homePageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-                        NavigationItem::make()->label('Tour page')
-                            ->url("$appUrl/admin/tour-the-terminal-contents/$tourPageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-                        NavigationItem::make()->label('Our story page')
-                            ->url("$appUrl/admin/our-story-contents/$OurStoryPageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-                        NavigationItem::make()->label('Services page')
-                            ->url("$appUrl/admin/services-contents/$servicesPageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-                        NavigationItem::make()->label('General aviation page')
-                            ->url("$appUrl/admin/general-aviation-contents/$generalAviationPageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-                        NavigationItem::make()->label('Elite services page')
-                            ->url("$appUrl/admin/elite-services-contents/$eliteServicesPageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-                        NavigationItem::make()->label('Contact us page')
-                            ->url("$appUrl/admin/contact-us-contents/$contactUsPageContentId/edit")
-                            ->icon('heroicon-o-collection')
-                            ->group("Website Content"),
-
-                    ]);
+                        ]);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
